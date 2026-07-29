01e88ec14f29596c16b1fe6e976055cd2ba1b3e6
Haerriz <29945587+haerriz@users.noreply.github.com>
2026-07-29 17:43:20 +0000

    Complete P1 job history actions

---
+++ Ui/Component/Listing/Column/JobActions.php
@@ -0,0 +1,46 @@
+<?php
+namespace Haerriz\GoogleShoppingFeed\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class JobActions extends Column
+{
+    private $urlBuilder;
+
+    public function __construct(
+        \Magento\Framework\View\Element\UiComponent\ContextInterface $context,
+        \Magento\Framework\View\Element\UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        foreach ($dataSource['data']['items'] ?? [] as &$item) {
+            $id = (int)$item['job_id'];
+            $item[$this->getData('name')] = [
+                'download' => [
+                    'href' => $this->urlBuilder->getUrl('*/*/download', ['id' => $id]),
+                    'label' => __('Download'),
+                ],
+                'retry' => [
+                    'href' => $this->urlBuilder->getUrl('*/*/retry', ['id' => $id]),
+                    'label' => __('Retry'),
+                    'confirm' => ['title' => __('Retry job'), 'message' => __('Generate this profile again?')],
+                    'post' => true,
+                ],
+                'cancel' => [
+                    'href' => $this->urlBuilder->getUrl('*/*/cancel', ['id' => $id]),
+                    'label' => __('Cancel'),
+                    'post' => true,
+                ],
+            ];
+        }
+        return $dataSource;
+    }
+}
