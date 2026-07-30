1a143fcc1788d989ef0d7f2cbc707d2d55bc611b
Haerriz <haerriz@gmail.com>
2026-07-30 12:51:10 +0000

    feat: Add Actions column, sorting controls, and 11 channel templates (Instagram, Snapchat, TikTok, Pinterest, Bing, Amazon, eBay, Rakuten, OpenAI)

--- Model/Template/Registry.php
+++ Model/Template/Registry.php
@@ -12,6 +12,15 @@ class Registry implements FeedTemplateRegistryInterface
     {
         $this->templates['google_shopping_v1'] = new Google\ShoppingV1();
         $this->templates['meta_catalog_v1'] = new Meta\CatalogV1();
+        $this->templates['instagram_catalog_v1'] = new Instagram\CatalogV1();
+        $this->templates['snapchat_catalog_v1'] = new Snapchat\CatalogV1();
+        $this->templates['tiktok_catalog_v1'] = new TikTok\CatalogV1();
+        $this->templates['pinterest_catalog_v1'] = new Pinterest\CatalogV1();
+        $this->templates['microsoft_merchant_v1'] = new Microsoft\MerchantV1();
+        $this->templates['amazon_catalog_v1'] = new Amazon\CatalogV1();
+        $this->templates['ebay_inventory_v1'] = new Ebay\InventoryV1();
+        $this->templates['rakuten_catalog_v1'] = new Rakuten\CatalogV1();
+        $this->templates['openai_commerce_v1'] = new OpenAi\CommerceV1();
     }
 
     public function getTemplate(string $code): FeedTemplateInterface
--- Ui/Component/Listing/Column/FeedActions.php
+++ Ui/Component/Listing/Column/FeedActions.php
@@ -31,6 +31,22 @@ class FeedActions extends Column
                     'href' => $this->urlBuilder->getUrl('*/*/edit', ['id' => $id]),
                     'label' => __('Edit'),
                 ],
+                'quick_view' => [
+                    'href' => $this->urlBuilder->getUrl('*/*/preview', ['id' => $id]),
+                    'label' => __('Quick View'),
+                ],
+                'generate' => [
+                    'href' => $this->urlBuilder->getUrl('*/*/trigger', ['id' => $id]),
+                    'label' => __('Generate Now'),
+                    'confirm' => [
+                        'title' => __('Generate Feed'),
+                        'message' => __('Trigger feed generation for %1?', $item['name'] ?? '')
+                    ]
+                ],
+                'duplicate' => [
+                    'href' => $this->urlBuilder->getUrl('*/*/duplicate', ['id' => $id]),
+                    'label' => __('Duplicate'),
+                ],
                 'history' => [
                     'href' => $this->urlBuilder->getUrl(
                         'haerriz_googleshoppingfeed/job/index',
@@ -40,8 +56,16 @@ class FeedActions extends Column
                 ],
                 'download' => [
                     'href' => $this->urlBuilder->getUrl('*/*/download', ['id' => $id]),
-                    'label' => __('Download Current Feed'),
+                    'label' => __('Download Feed'),
                 ],
+                'delete' => [
+                    'href' => $this->urlBuilder->getUrl('*/*/delete', ['id' => $id]),
+                    'label' => __('Delete'),
+                    'confirm' => [
+                        'title' => __('Delete Feed Profile'),
+                        'message' => __('Are you sure you want to delete %1?', $item['name'] ?? '')
+                    ]
+                ]
             ];
         }
         unset($item);
