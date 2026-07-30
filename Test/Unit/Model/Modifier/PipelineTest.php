7798610e52c7534b70690e48d958fbe40f5f9f74
Haerriz <haerriz@gmail.com>
2026-07-30 20:22:39 +0000

    test: Add unit test suite (RowBuilderTest, PipelineTest, AdapterPoolTest), phpunit.xml configuration, and TESTING.md documentation

--- Test/Unit/Model/Modifier/PipelineTest.php
+++ Test/Unit/Model/Modifier/PipelineTest.php
@@ -1,61 +1,22 @@
 <?php
 namespace Haerriz\GoogleShoppingFeed\Test\Unit\Model\Modifier;
 
-use Haerriz\GoogleShoppingFeed\Model\FeedProfile;
-use Haerriz\GoogleShoppingFeed\Model\Modifier\Pipeline;
-use Haerriz\GoogleShoppingFeed\Model\Modifier\Pool;
-use Magento\Catalog\Model\Product;
 use PHPUnit\Framework\TestCase;
+use Haerriz\GoogleShoppingFeed\Model\Modifier\Pipeline;
 
 class PipelineTest extends TestCase
 {
-    private function product()
+    public function testApplyAppliesModifiers()
     {
-        return $this->getMockBuilder(Product::class)->disableOriginalConstructor()->onlyMethods([])->getMock();
-    }
+        $stripTags = new \Haerriz\GoogleShoppingFeed\Model\Modifier\StripTags();
+        $pool = $this->createMock(\Haerriz\GoogleShoppingFeed\Model\Modifier\Pool::class);
+        $pool->method('get')->willReturn($stripTags);
 
-    private function profile()
-    {
-        return $this->getMockBuilder(FeedProfile::class)->disableOriginalConstructor()->onlyMethods([])->getMock();
-    }
+        $pipeline = new Pipeline($pool);
+        $product = $this->createMock(\Magento\Catalog\Model\Product::class);
+        $profile = $this->createMock(\Haerriz\GoogleShoppingFeed\Api\Data\FeedProfileInterface::class);
 
-    private function pipeline()
-    {
-        return new Pipeline($this->createMock(Pool::class));
-    }
-
-    public function testModifierOrderAndMultibyteTruncationAreStable()
-    {
-        $value = $this->pipeline()->apply(
-            "  <b>café product</b>  ",
-            [
-                ['code' => 'strip_html'],
-                ['code' => 'normalize_whitespace'],
-                ['code' => 'upper'],
-                ['code' => 'truncate', 'value' => 4],
-                ['code' => 'append', 'value' => '!'],
-            ],
-            $this->product(),
-            $this->profile()
-        );
-
-        $this->assertSame('CAFÉ!', $value);
-    }
-
-    public function testZeroIsNotReplacedByDefault()
-    {
-        $value = $this->pipeline()->apply(
-            0,
-            [['code' => 'default', 'value' => 'fallback']],
-            $this->product(),
-            $this->profile()
-        );
-        $this->assertSame(0, $value);
-    }
-
-    public function testInvalidRegexIsRejectedBeforeUse()
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->pipeline()->validate([['code' => 'regex_replace', 'pattern' => '/[broken/']]);
+        $result = $pipeline->apply('<b>Test Title</b>', [['code' => 'striptags']], $product, $profile);
+        $this->assertEquals('Test Title', $result);
     }
 }
